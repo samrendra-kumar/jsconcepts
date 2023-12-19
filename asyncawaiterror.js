ff6b8bb10557936fbb1b01498fed13566a20cb8d
Samrendra <samrendrakumar893@gmail.com>
2023-12-19 10:48:02 +0530

    async error added

---
+++ asyncawaiterror.js
@@ -0,0 +1,13 @@
+async function processingtask(num)
+{
+    try{
+      if(num==7) 
+      return "the number is 7 "  ;
+    }catch(error)
+    {
+     console.log("error is ",error)
+    }
+}
+y =processingtask(8) ;
+console.log(y);
+
