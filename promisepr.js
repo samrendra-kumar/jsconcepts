ee12986b11280470614ff539daec9539799d30df
Samrendra <samrendrakumar893@gmail.com>
2023-12-21 12:02:57 +0530

    Added nested chain of promie

--- promisepr.js
+++ promisepr.js
@@ -1,16 +1,3 @@
-// A promise is a special Js Objects that also  are considered as a
-// readibility enhancer .They get immediately returned from a function 
-// setup to return a promise WE can also add a functionality we want to add
-// until the future task is done .A promise is in one of these states 
-//  . pending : initial state ,neither fulfilled nor rejected .
-//  . fulfilled : meaning that the operation was completed successfully .
-//  .rejected : meaning that operation failed .
-
-//  what's the main reason of using promise  ? 
-//   .The first reason is prevention of callback hell but for that only 
-//    promise is not used bcoz if we will us promise then there is also a chance of promise hell .
-//   . the most valid reason  is inversion of control withthe use of this problem is 
-//    eliminated .
 
    //declaration of promise 
 
